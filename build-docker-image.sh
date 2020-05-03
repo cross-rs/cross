d62c9f7d9570adfdd4bc9e6564218e1b9ba09f49
Markus Reiter <me@reitermark.us>
2020-05-03 15:10:08 +0200

    Run ShellCheck on CI.

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,20 +3,20 @@
 set -x
 set -euo pipefail
 
+version="$(cargo metadata --format-version 1 | jq --raw-output '.packages[] | select(.name == "cross") | .version')"
+
 cd docker
 
 run() {
   local dockerfile="Dockerfile.${1}"
   local image_name="rustembedded/cross:${1}"
-  local cache_from_args=
+  local cache_from_args=()
 
   if docker image inspect "${image_name}" &>/dev/null || docker pull "${image_name}"; then
     cache_from_args=(--cache-from "${image_name}")
   fi
 
-  docker build ${cache_from_args[@]} --pull -t "${image_name}" -f "${dockerfile}" .
-
-  local version="$(cargo metadata --format-version 1 | jq --raw-output '.packages[] | select(.name == "cross") | .version')"
+  docker build "${cache_from_args[@]}" --pull -t "${image_name}" -f "${dockerfile}" .
 
   if ! [[ "${version}" =~ alpha ]] && ! [[ "${version}" =~ dev ]]; then
     local versioned_image_name="${image_name}-${version}"
@@ -24,7 +24,7 @@ run() {
   fi
 }
 
-if [ -z "${@:-}" ]; then
+if [[ -z "${*}" ]]; then
   for t in Dockerfile.*; do
     run "${t##Dockerfile.}"
   done
