da74ad3f4b41eb48ae413cbc6f334fa3c748da89
David Laban <alsuren@gmail.com>
2020-12-23 18:34:14 +0000

    remove dead code

--- src/docker.rs
+++ src/docker.rs
@@ -240,7 +240,7 @@ fn build_docker_image(toml: &Toml, target: &Target, verbose: bool) -> Result<Str
 
 pub fn image(toml: Option<&Toml>, target: &Target, verbose: bool) -> Result<String> {
     if let Some(toml) = toml {
-        match (toml.image(target)?.map(|s| s.to_owned()), toml.context(target)?) {
+        match (toml.image(target)?, toml.context(target)?) {
             (Some(_image), Some(_context)) => {
                 // https://docs.docker.com/compose/compose-file/#build says:
                 // If you specify image as well as build, then Compose names the
@@ -248,14 +248,9 @@ pub fn image(toml: Option<&Toml>, target: &Target, verbose: bool) -> Result<Stri
                 // This is not supported yet, so the best thing to do is to explode.
                 bail!("Specifying `image` and `context` in Cross.toml is not yet supported.")
             },
-            (Some(image), None) => return Ok(image),
+            (Some(image), None) => return Ok(image.to_owned()),
             (None, Some(_context)) => return build_docker_image(toml, target, verbose),
             (None, None) => (),
-        }
-        if let Some(image) = toml.image(target)?.map(|s| s.to_owned()) {
-            return Ok(image)
-        } else if let Some(_) = toml.context(target)? {
-            return build_docker_image(toml, target, verbose)
         }
     }
 
