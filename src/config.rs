561859c1ac43fe7d60ebdccb1a250919a6cb4327
Oliver <oliver.thallmair@mailbox.org>
2021-03-08 08:43:58 +0100

    cleanup format

--- src/config.rs
+++ src/config.rs
@@ -6,7 +6,6 @@ use std::env::var;
 struct Environment(&'static str, Option<HashMap<&'static str, &'static str>>);
 
 impl Environment {
-    
     fn new() -> Self {
         Environment("CROSS", None)
     }
@@ -26,14 +25,19 @@ impl Environment {
     fn get_var(&self, name: &str) -> Option<String> {
         var(name).ok().and_then(|v| Some(v))
     }
+
     #[cfg(test)]
     /// for tests get value from internal map
     fn get_var(&self, name: &str) -> Option<String> {
-        self.1.as_ref().and_then(|map| map.get(name).and_then(|v| Some(v.to_string())))
+        self.1
+            .as_ref()
+            .and_then(|map| map.get(name).and_then(|v| Some(v.to_string())))
     }
+
     fn target_path(target: &Target, key: &str) -> String {
         format!("TARGET_{}_{}", target.triple(), key)
     }
+
     fn build_path(key: &str) -> String {
         format!("BUILD_{}", key)
     }
@@ -74,35 +78,41 @@ impl Environment {
 
         Ok((build_env, target_env))
     }
+
     fn image(&self, target: &Target) -> Option<String> {
         self.get_target_var(target, "IMAGE")
     }
+
     fn runner(&self, target: &Target) -> Option<String> {
         self.get_target_var(target, "RUNNER")
     }
 
     fn passthrough(&self, target: &Target) -> (Option<Vec<String>>, Option<Vec<String>>) {
-        let passthrough_target = self.get_target_var(target, "env_passthrough")
-        .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
-        
-        let passthrough_build = self.get_build_var("env_passthrough")
-        .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
+        let passthrough_target = self
+            .get_target_var(target, "env_passthrough")
+            .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
+
+        let passthrough_build = self
+            .get_build_var("env_passthrough")
+            .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
 
         (passthrough_build, passthrough_target)
     }
 
     fn volumes(&self, target: &Target) -> (Option<Vec<String>>, Option<Vec<String>>) {
-        let volumes_target = self.get_target_var(target, "ENV_VOLUMES")
-        .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
-        
-        let volumes_build = self.get_build_var("ENV_VOLUMES")
-        .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
+        let volumes_target = self
+            .get_target_var(target, "ENV_VOLUMES")
+            .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
+
+        let volumes_build = self
+            .get_build_var("ENV_VOLUMES")
+            .and_then(|ref s| Some(split_to_cloned_by_ws(s)));
 
         (volumes_build, volumes_target)
     }
 }
 
-fn split_to_cloned_by_ws(string : &str) -> Vec<String> {
+fn split_to_cloned_by_ws(string: &str) -> Vec<String> {
     string.split_whitespace().map(|v| v.to_string()).collect()
 }
 
@@ -119,14 +129,12 @@ impl Config {
             env: Environment::new(),
         }
     }
-    
+
     #[cfg(test)]
     fn new_with(toml: Option<Toml>, env: Environment) -> Self {
-        Config {
-            toml,
-            env,
-        }
+        Config { toml, env }
     }
+
     pub fn xargo(&self, target: &Target) -> Result<Option<bool>> {
         let (build_xargo, target_xargo) = self.env.xargo(target)?;
         let (toml_build_xargo, toml_target_xargo) = if let Some(ref toml) = self.toml {
@@ -148,6 +156,7 @@ impl Config {
         };
         Ok(None)
     }
+
     pub fn image(&self, target: &Target) -> Result<Option<String>> {
         let env_value = self.env.image(target);
         if let Some(env_value) = env_value {
@@ -155,6 +164,7 @@ impl Config {
         }
         self.toml.as_ref().map_or(Ok(None), |t| t.image(target))
     }
+
     pub fn runner(&self, target: &Target) -> Result<Option<String>> {
         let env_value = self.env.runner(target);
         if let Some(env_value) = env_value {
@@ -218,8 +228,8 @@ impl Config {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Target, TargetList};
     use super::*;
+    use crate::{Target, TargetList};
 
     fn target() -> Target {
         let target_list = TargetList {
@@ -232,7 +242,6 @@ mod tests {
 
         use super::*;
 
-        
         #[test]
         pub fn parse_error_in_env() {
             let mut map = std::collections::HashMap::new();
@@ -299,14 +308,12 @@ mod tests {
             );
         }
     }
-       
 
     mod test_config {
 
         use super::*;
         use std::matches;
 
-       
         fn toml(content: &str) -> Result<crate::Toml> {
             Ok(crate::Toml {
                 table: if let Ok(toml::Value::Table(table)) = content.parse() {
@@ -328,18 +335,19 @@ mod tests {
 
             Ok(())
         }
+
         #[test]
         pub fn env_target_and_toml_target_xargo_target_then_use_env() -> Result<()> {
             let mut map = HashMap::new();
             map.insert("CROSS_TARGET_AARCH64_UNKNOWN_LINUX_GNU_XARGO", "true");
             let env = Environment::new_with(map);
-            
-            
+
             let config = Config::new_with(Some(toml(TOML_TARGET_XARGO_FALSE)?), env);
             assert!(matches!(config.xargo(&target()), Ok(Some(true))));
-            
+
             Ok(())
         }
+        
         #[test]
         pub fn env_target_and_toml_build_xargo_then_use_toml() -> Result<()> {
             let mut map = HashMap::new();
