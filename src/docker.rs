aba2365b644fd27d118a4bd611342dc1059d0731
dalance <dalance@gmail.com>
2022-03-14 14:36:39 +0900

    Add privileged to docker

--- src/docker.rs
+++ src/docker.rs
@@ -168,6 +168,7 @@ pub fn run(
     docker.args(&["-e", "PKG_CONFIG_ALLOW_CROSS=1"]);
 
     docker.arg("--rm");
+    docker.arg("--privileged");
 
     // We need to specify the user for Docker, but not for Podman.
     if let Ok(ce) = get_container_engine() {
