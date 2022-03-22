5add7f98ae3f625314b9587f17ba54571b6932d5
Niklas Kunz <git@monoton.space>
2022-03-22 16:55:51 +0100

    Add basic documentation

--- src/cross_toml.rs
+++ src/cross_toml.rs
@@ -1,3 +1,39 @@
+//! Implements the parsing of `Cross.toml`
+//!
+//! `Cross.toml` can contain the following elements:
+//!
+//! # `build`
+//! The `build` key allows you to set global variables, e.g.:
+//!
+//! ```toml
+//! [build]
+//! xargo = true
+//! ```
+//!
+//! # `build.env`
+//! With the `build.env` key you can globally set volumes that should be mounted
+//! in the Docker container or environment variables that should be passed through.
+//! For example:
+//!
+//! ```toml
+//! [build.env]
+//! volumes = ["vol1", "vol2"]
+//! passthrough = ["IMPORTANT_ENV_VARIABLES"]
+//! ```
+//!
+//! # `target.TARGET`
+//! The `target` key allows you to specify parameters for specific compilation targets.
+//!
+//! ```toml
+//! [target.aarch64-unknown-linux-gnu]
+//! volumes = ["vol1", "vol2"]
+//! passthrough = ["VAR1", "VAR2"]
+//! xargo = false
+//! image = "test-image"
+//! runner = "custom-runner"
+//! ```
+//! 
+
 use std::collections::HashMap;
 use serde::Deserialize;
 use crate::errors::*;
@@ -31,7 +67,7 @@ pub struct CrossTargetConfig {
 /// Wrapper struct for `Target` -> `CrossTargetConfig` mappings
 ///
 /// This is used to circumvent that serde's flatten and field aliases
-/// currently don't work together: https://github.com/serde-rs/serde/issues/1504
+/// currently don't work together: <https://github.com/serde-rs/serde/issues/1504>
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct CrossTargetMapConfig {
     #[serde(flatten)]
