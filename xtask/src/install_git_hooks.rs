65add5b48040987e9a92cf8f7f29f32e25e3c7c8
Emil Gardström <emil.gardstrom@gmail.com>
2022-07-06 15:22:57 +0200

    use proper .git folder

--- xtask/src/install_git_hooks.rs
+++ xtask/src/install_git_hooks.rs
@@ -1,6 +1,8 @@
+use std::{path::PathBuf, process::Command};
+
 use crate::util::project_dir;
 use clap::Args;
-use cross::shell::MessageInfo;
+use cross::{shell::MessageInfo, CommandExt};
 
 #[derive(Args, Debug)]
 pub struct InstallGitHooks {
@@ -17,7 +19,11 @@ pub struct InstallGitHooks {
 
 pub fn install_git_hooks(msg_info: &mut MessageInfo) -> cross::Result<()> {
     let root = project_dir(msg_info)?;
-    let git_hooks = root.join(".git").join("hooks");
+    let git_hooks = Command::new("git")
+        .args(&["rev-parse", "--git-common-dir"])
+        .run_and_get_stdout(msg_info)
+        .map(|s| PathBuf::from(&s.trim()))?
+        .join("hooks");
     let cross_dev = root.join("xtask").join("src");
 
     std::fs::copy(
