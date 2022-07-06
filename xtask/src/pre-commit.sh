e7abd2cd832c7a48295518b1e1128d05f1bb094b
Emil Gardström <emil.gardstrom@gmail.com>
2022-07-06 15:46:10 +0200

    only check staged content

--- xtask/src/pre-commit.sh
+++ xtask/src/pre-commit.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+set -ex
+
+tf=$(mktemp -t stash.XXX.$$)
+
+cleanup() {
+    git apply --allow-empty --whitespace=nowarn < "$tf" && git stash drop -q
+    rm "$tf"
+}
+
+git diff --full-index --binary > "$tf"
+git stash -q --keep-index
+
+trap cleanup EXIT
+
 cargo xtask check --verbose
