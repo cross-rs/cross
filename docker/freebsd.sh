1d942e42a30bc3be217593eb091c1780a5e871f0
Alex Huszagh <ahuszagh@gmail.com>
2022-07-15 00:53:49 -0500

    Remove OpenSSL and SQLite3 from FreeBSD images.

--- docker/freebsd.sh
+++ docker/freebsd.sh
@@ -5,9 +5,19 @@ set -euo pipefail
 
 export ARCH="${1}"
 # shellcheck disable=SC1091
-. freebsd-common.sh
-# shellcheck disable=SC1091
 . lib.sh
+
+export BSD_ARCH=
+case "${ARCH}" in
+    x86_64)
+        BSD_ARCH=amd64
+        ;;
+    i686)
+        BSD_ARCH=i386
+        ;;
+esac
+export BSD_HOME="ftp.freebsd.org/pub/FreeBSD/releases"
+export BSD_MAJOR=12
 
 max_freebsd() {
     local best=
