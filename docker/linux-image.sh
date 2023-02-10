8fbe4ef0e4b8a48677831e01503ed437d9211178
Emil Gardström <emil.gardstrom@gmail.com>
2023-02-10 19:34:52 +0100

    add debian 2023 keys

--- docker/linux-image.sh
+++ docker/linux-image.sh
@@ -201,7 +201,7 @@ main() {
     curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{7.0,8,9,10,11}.asc' -O
     curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{8,9,10,11}-security.asc' -O
     curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/release-{7,8,9,10,11}.asc' -O
-    curl --retry 3 -sSfL 'https://www.ports.debian.org/archive_{2020,2021,2022}.key' -O
+    curl --retry 3 -sSfL 'https://www.ports.debian.org/archive_{2020,2021,2022,2023}.key' -O
 
     for key in *.asc *.key; do
         apt-key add "${key}"
